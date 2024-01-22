9829731d1e77c331cb264556fefec1572422ea13
Benedikt <b.schesch@gmail.com>
2024-01-22 15:11:07 -0800

    Base

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.charset.StandardCharsets;
+
+public class main {
+    public void saveAll(Path root) {
+        for(Map.Entry<Path, ParseResult<CompilationUnit>> 
+            cu:content.entrySet()){
+            final Path path=root.resolve(cu.getKey()); 
+            final String code=new PrettyPrinter()
+                .print(cu.getValue().getResult().get()); 
+            try(PrintWriter out=
+                new PrintWriter(path.toFile(), UTF8.toString())){
+                out.println(code); 
+            }
+        } 
+    }
+}
+
